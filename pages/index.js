38bec2ae8d935c7b7036f9086251c2cb02ab46f1
Leonardo dos Santos <leo.santos2001@hotmail.com>
2025-11-06 23:47:44 -0300

    adiciona o arquivo para o desafio - teste concluido

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <html><h1 style="{background-color: black}">VAII CORINTHIANS!</h1><a href="https://www.youtube.com/watch?v=9cIWcoQONfk"></a></html>;
+    return <a href="https://www.youtube.com/watch?v=9cIWcoQONfk">VAI CORINTHIANS</a>
 }
 
 export default Home;
