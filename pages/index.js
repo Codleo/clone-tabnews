b4d5e1f3d0af1c5cdd25c21e541ceb4d51f7eab7
Leonardo dos Santos <leo.santos2001@hotmail.com>
2026-05-17 05:29:18 -0300

    link do ver status na home

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,11 @@
+import Link from "next/link";
 function Home() {
   return (
-    <a href="https://www.youtube.com/watch?v=9cIWcoQONfk">VAI CORINTHIANS</a>
+    <>
+      <a href="https://www.youtube.com/watch?v=9cIWcoQONfk">VAI CORINTHIANS</a>
+      <br />
+      <Link href="/api/v1/status">Ver status</Link>
+    </>
   );
 }
 
