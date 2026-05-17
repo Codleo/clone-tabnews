56ef38118a652f8042c9b48c93d7194478a0aa30
Leonardo dos Santos <leo.santos2001@hotmail.com>
2026-05-17 05:21:10 -0300

    código funcionando e conectado ao neon

--- infra/database.js
+++ infra/database.js
@@ -7,16 +7,19 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
+    ssl: {
+      rejectUnauthorized: false, // necessário para o Neon
+    },
   });
 
-  console.log("Credenciais do Postgres:", {
+  /* console.log("Credenciais do Postgres:", {
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
   });
-
+*/
   try {
     await client.connect();
     const result = await client.query(queryObject);
